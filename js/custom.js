fd0a4b2e3616268fba304aa06028565950c9e9f1
Baburao <dhirajsapkal@gmail.com>
2014-11-25 10:41:02 +0530

    Added the affix class to the nav bar, but still not working as desiered. Also changed bg

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,12 @@
 $(document).ready(function() {
   $('#scene').parallax();
 });
+
+$('#myAffix').affix({
+  offset: {
+    top: 100,
+    bottom: function () {
+      return (this.bottom = $('.footer').outerHeight(true))
+    }
+  }
+})
