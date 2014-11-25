184b8eca54224f1496f9e1a60faa1a6da372d3f6
Baburao <dhirajsapkal@gmail.com>
2014-11-25 22:59:36 +0530

    managed to place background behind, now just to fit it

--- js/custom.js
+++ js/custom.js
@@ -1,12 +1,3 @@
 $(document).ready(function() {
   $('#scene').parallax();
 });
-
-$('#myAffix').affix({
-  offset: {
-    top: 100,
-    bottom: function () {
-      return (this.bottom = $('.footer').outerHeight(true))
-    }
-  }
-})
